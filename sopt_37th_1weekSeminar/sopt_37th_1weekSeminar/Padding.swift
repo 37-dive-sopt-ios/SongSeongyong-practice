4e4b7d07e2e7156aab274d29bf0379cd65261504
soseoyo12 <songsy0612@gmail.com>
2025-10-16 21:07:53 +0900

    [Feat] Button Attribute Application
    리퀴드 글래스 스타일 적용해봤습니다.

---
+++ sopt_37th_1weekSeminar/sopt_37th_1weekSeminar/Padding.swift
@@ -0,0 +1,22 @@
+//
+//  Padding.swift
+//  sopt_37th_1weekSeminar
+//
+//  Created by 송성용 on 10/16/25.
+//
+import UIKit
+
+
+extension UITextField {
+    func addLeftPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+    
+    func addRightPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.rightView = paddingView
+        self.rightViewMode = ViewMode.always
+    }
+}
